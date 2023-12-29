7c2d907c688284e175bc20e64c0ae914b46b9054
Heitor da Costa Cândido <151862970+Hekth@users.noreply.github.com>
2023-12-29 13:38:47 -0300

    adicionando icone de duvida
    adiciona icone de interrogação que mostra o tutorial novamente

--- js/tutorial.js
+++ js/tutorial.js
@@ -2,19 +2,22 @@ const dialog = document.querySelectorAll('dialog');
 const btnPular = document.querySelectorAll('.btn__pular');
 const btnProximo = document.querySelectorAll('.btn__proximo');
 const btnJogar = document.querySelector('.btn-jogar');
+const duvida = document.querySelector('.cabecalho__duvida');
 
 const mostrarTutorial = localStorage.getItem('mostrarTutorial') || null;
+
+btnJogar.addEventListener('click', () => fechaModal());
+    
+btnPular.forEach((btn, index) => {
+    btn.addEventListener('click', () => fechaModal());
+    btnProximo[index].addEventListener('click', () => proximoModal(index));
+});
+
+duvida.addEventListener('click', () => dialog[0].showModal());
 
 if (!mostrarTutorial) {
     dialog[0].showModal();
     
-    btnJogar.addEventListener('click', () => fechaModal());
-    
-    btnPular.forEach((btn, index) => {
-        btn.addEventListener('click', () => fechaModal());
-        btnProximo[index].addEventListener('click', () => proximoModal(index));
-    });
-
     localStorage.setItem('mostrarTutorial', 'no');
 }
 
